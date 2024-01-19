b7d1572ce437deb2e20950d4343aa67bb0ec57d3
Yoshihito Ishihara <yoshihito.093079@gmail.com>
2024-01-20 00:09:54 +0900

    first commit of ReactApp

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReservationForm from './components/ReservationForm';
+
+function App() {
+    return (
+        <div className="App">
+            <ReservationForm />
+        </div>
+    );
+}
+
+export default App;
