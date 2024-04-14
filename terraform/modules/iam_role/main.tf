4d92c49691346cfc75388040da8a566f70228840
Yoshihito Ishihara <yoshihito.093079@gmail.com>
2024-04-15 01:35:54 +0900

    add iam_policy and iam_role

--- terraform/modules/iam_role/main.tf
+++ terraform/modules/iam_role/main.tf
@@ -1,4 +1,4 @@
-
+# CloudFront invalidation cash Policy
 data "aws_iam_policy_document" "cloudfront_cash_invalidation" {
     statement {
 			effect   = "Allow"
@@ -11,3 +11,29 @@ resource "aws_iam_policy" "add_create_invalidation" {
     name   = "add_create_invalidation"
     policy = data.aws_iam_policy_document.cloudfront_cash_invalidation.json
 }
+
+# SES send Policy
+data "aws_iam_policy_document" "ses_send_mail" {
+    statement {
+			effect   = "Allow"
+			action   = ["ses:SendEmail", "ses:SendRawEmail"] 
+			resource = ["*"]
+		}
+}
+
+# AWS管理ポリシー(SESFullAccess)
+data "aws_iam_policy" "AmazonSESFullAccess" {
+    arn = "arn:aws:iam::aws:policy/AmazonSESFullAccess"
+}
+
+# AWS管理ポリシー(LambdaBasicExecutionRole)
+data "aws_iam_policy" "AmazonSESFullAccess" {
+    arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+
+resource "aws_iam_policy" "SendSESPolicy" {
+    name   = "SendSESPolicy"
+    policy = data.aws_iam_policy_document.ses_send_mail.json
+}
+
