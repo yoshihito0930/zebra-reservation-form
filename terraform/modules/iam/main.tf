b9733f0f67c6842d05dbbfb7c68c41fb9342452b
Yoshihito Ishihara <yoshihito.093079@gmail.com>
2024-06-11 00:51:34 +0900

    add IAM Groupes

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -27,17 +27,17 @@ resource "aws_iam_access_key" "admin-key2" {
 
 
 resource "aws_iam_user" "github-s3" {
-  force_destroy = "false"
-  name          = var.githubAction-user
-  path          = "/"
+    force_destroy = "false"
+    name          = var.githubAction-user
+    path          = "/"
 
-  tags = {
-    AKIA3X6SIRR5JIEQYVVF = "for github access_key"
-  }
+    tags = {
+        AKIA3X6SIRR5JIEQYVVF = "for github access_key"
+    }
 
-  tags_all = {
-    AKIA3X6SIRR5JIEQYVVF = "for github access_key"
-  }
+    tags_all = {
+        AKIA3X6SIRR5JIEQYVVF = "for github access_key"
+    }
 }
 
 resource "aws_iam_access_key" "github-s3-key1" {
@@ -45,7 +45,16 @@ resource "aws_iam_access_key" "github-s3-key1" {
     status     = "Active"
 }
 
+# IAM Groupe
+resource "aws_iam_group" "administrator" {
+    name = "administrator"
+    path = "/"
+}
 
+resource "aws_iam_group_policy_attachment" "AdministratorAccess" {
+    group      = aws_iam_group.administrator
+    policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
 
 # IAM Role
 
